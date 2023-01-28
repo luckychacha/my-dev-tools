9ea976279e41265fd194a89a6cd3900e3b6ff84f
luckychacha <358140284@qq.com>
2023-01-28 18:32:35 +0800

    Edit output.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 pub mod base64;
 pub mod error;
 
-use clap::Parser;
+use clap::{command, Arg, Command, Parser};
 
 use crate::base64::{Base64Input, Base64Output};
 
-/// Try to make an tool sets for daily develop.
 #[derive(Parser, Debug)]
-#[clap(version = "0.1", author = "Luckychacha <luckychachaa@gmail.com>")]
 struct Args {
     #[command(subcommand)]
     action: Action,
@@ -21,10 +19,35 @@ enum Action {
     Base64Decode { input: String },
 }
 fn main() {
-    let args = Args::parse();
-    match args.action {
-        Action::Base64Decode { input } => {
-            let result = input.as_str().parse::<Base64Input>();
+    let matches = Command::new("my-dev-tools")
+        .author("luckychacha")
+        .version("0.1.0")
+        .about("Try to make an tool sets for daily develop.")
+        .subcommand(
+            Command::new("Base64Encode")
+                .about("Try to generate a base64 encoded string. Such as \"my-dev-tools base64-encode 'hello world!'\"")
+                .arg(Arg::new("input")),
+        )
+        .subcommand(
+            Command::new("Base64Decode")
+                .about("Parse a base64 encoded string. Such as \"my-dev-tools base64-decode 'aGVsbG8gd29ybGQh'\"")
+                .arg(Arg::new("input")),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("Base64Encode") {
+        if let Some(s) = matches.get_one::<String>("input") {
+            if let Ok(base64_encoded) = s.as_str().parse::<Base64Output>() {
+                println!("Input is: {}", base64_encoded.raw);
+                println!(
+                    "Base64 encode result is: \"{}\"",
+                    base64_encoded.base64_encoded
+                );
+            }
+        }
+    } else if let Some(matches) = matches.subcommand_matches("Base64Decode") {
+        if let Some(s) = matches.get_one::<String>("input") {
+            let result = s.as_str().parse::<Base64Input>();
             match result {
                 Ok(base64_decoded) => {
                     println!(
@@ -39,14 +62,33 @@ fn main() {
                 }
             }
         }
-        Action::Base64Encode { input } => {
-            if let Ok(base64_encoded) = input.as_str().parse::<Base64Output>() {
-                println!("Input is: {}", base64_encoded.raw);
-                println!(
-                    "Base64 encode result is: \"{}\"",
-                    base64_encoded.base64_encoded
-                );
-            }
-        }
     }
+    // let args = Args::parse();
+    // match args.action {
+    //     Action::Base64Decode { input } => {
+    //         let result = input.as_str().parse::<Base64Input>();
+    //         match result {
+    //             Ok(base64_decoded) => {
+    //                 println!(
+    //                     "Base64 decode result is: {}",
+    //                     base64_decoded
+    //                         .translate_into_human_readable_content()
+    //                         .unwrap_or(String::from("Bytes convert to string error."))
+    //                 )
+    //             }
+    //             Err(error) => {
+    //                 println!("Parse input error: {error:?}")
+    //             }
+    //         }
+    //     }
+    //     Action::Base64Encode { input } => {
+    //         if let Ok(base64_encoded) = input.as_str().parse::<Base64Output>() {
+    //             println!("Input is: {}", base64_encoded.raw);
+    //             println!(
+    //                 "Base64 encode result is: \"{}\"",
+    //                 base64_encoded.base64_encoded
+    //             );
+    //         }
+    //     }
+    // }
 }
