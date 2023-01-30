35cce848ef1c036403b15a74a73415bc9eadb02b
luckychacha <358140284@qq.com>
2023-01-30 11:34:29 +0800

    [My-Dev-Tools] Day3: Add Command and MyArgMatches types and make the lib.rs cleaner.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 pub mod base64;
 pub mod error;
 pub mod types;
-use clap::{Arg, Command};
-use types::my_arg_matches::{MyArgMatches, SubCommandExt};
+use clap::Command;
+use types::{
+    command::CommandExt,
+    my_arg_matches::{MyArgMatches, SubCommandExt},
+};
 
 pub fn command_parse() {
     let matches = Command::new("my-dev-tools")
@@ -12,37 +15,4 @@ pub fn command_parse() {
 
     let my_matches = MyArgMatches(matches);
     my_matches.exec();
-}
-
-pub trait CommandExt {
-    fn add_basic_info(self) -> Self;
-    fn add_base64_subcommands(self) -> Self;
-}
-
-impl CommandExt for Command {
-    fn add_basic_info(self) -> Self {
-        self.author("luckychacha")
-            .version("0.1.0")
-            //         .help_template("\
-            // {before-help}{name} {version}
-            // {author-with-newline}{about-with-newline}
-            // {usage-heading} {usage}
-            // {all-args}{after-help}
-            // ")
-            .about("Try to make a tool sets for daily develop.")
-            .arg_required_else_help(true)
-    }
-
-    fn add_base64_subcommands(self) -> Self {
-        self.subcommand(
-            Command::new("base64-encode")
-                .about("Try to generate a base64 encoded string. Such as \"my-dev-tools base64-encode 'hello world!'\"")
-                .arg(Arg::new("input")),
-        )
-        .subcommand(
-            Command::new("base64-decode")
-                .about("Parse a base64 encoded string. Such as \"my-dev-tools base64-decode 'aGVsbG8gd29ybGQh'\"")
-                .arg(Arg::new("input")),
-        )
-    }
 }
---
+++ src/types/command.rs
@@ -0,0 +1,34 @@
+use clap::{Arg, Command};
+
+pub trait CommandExt {
+    fn add_basic_info(self) -> Self;
+    fn add_base64_subcommands(self) -> Self;
+}
+
+impl CommandExt for Command {
+    fn add_basic_info(self) -> Self {
+        self.author("luckychacha")
+            .version("0.1.0")
+            //         .help_template("\
+            // {before-help}{name} {version}
+            // {author-with-newline}{about-with-newline}
+            // {usage-heading} {usage}
+            // {all-args}{after-help}
+            // ")
+            .about("Try to make a tool sets for daily develop.")
+            .arg_required_else_help(true)
+    }
+
+    fn add_base64_subcommands(self) -> Self {
+        self.subcommand(
+            Command::new("base64-encode")
+                .about("Try to generate a base64 encoded string. Such as \"my-dev-tools base64-encode 'hello world!'\"")
+                .arg(Arg::new("input")),
+        )
+        .subcommand(
+            Command::new("base64-decode")
+                .about("Parse a base64 encoded string. Such as \"my-dev-tools base64-decode 'aGVsbG8gd29ybGQh'\"")
+                .arg(Arg::new("input")),
+        )
+    }
+}
