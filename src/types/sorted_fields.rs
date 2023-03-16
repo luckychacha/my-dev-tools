5168b7f6c8913cc827ede20465593245bd1e5872
luckychacha <358140284@qq.com>
2023-03-17 00:09:42 +0800

    Add test mod for sorted_fields trait.

--- src/types/sorted_fields.rs
+++ src/types/sorted_fields.rs
@@ -15,9 +15,44 @@ pub trait SortedFields {
                     query_string.push_str(&format!("{key}={}&", fields.get(key).unwrap()));
                     query_string
                 });
-        query_string.push_str(&format!("token={}", token));
+        query_string.push_str(&format!("token={token}"));
         query_string
     }
 }
 
 // todo: add tests
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct User {
+        pub id: usize,
+        pub name: String,
+        pub age: usize,
+        pub balance: i32,
+    }
+
+    impl SortedFields for User {
+        fn get_fields(&self, fields: &mut HashMap<String, String>) {
+            fields.insert(String::from("id"), 1.to_string());
+            fields.insert(String::from("name"), String::from("test"));
+            fields.insert(String::from("age"), 20.to_string());
+            fields.insert(String::from("balance"), 1000.to_string());
+        }
+    }
+
+    #[test]
+    fn sorted_fields_should_work() {
+        let user = User {
+            id: 1,
+            name: "test".to_string(),
+            age: 20,
+            balance: 1000,
+        };
+
+        assert_eq!(
+            user.sorted_fields("123"),
+            "age=20&balance=1000&id=1&name=test&token=123"
+        );
+    }
+}
